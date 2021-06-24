0127acbbebf636415e4487af6e7eb4df85eef270
brunaoo <bruno-alves90@hotmail.com>
2021-06-24 14:35:45 -0300

    classes de dominio

---
+++ src/main/java/com/brunao/bookstore/domain/Livro.java
@@ -0,0 +1,11 @@
+package com.brunao.bookstore.domain;
+
+public class Livro {
+
+	private Integer id;
+	private String titulo;
+	private String nomeAutor;
+	private String texto;
+	
+	private Categoria categoria;
+}
